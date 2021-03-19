bacc6be7fd81934ba90021c052c8f01f842ddc30
Wiktor de Pina <Wiktor.Depina@simplybusiness.co.uk>
2021-03-19 10:54:15 +0000

    more test for Aged Brie

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -2,77 +2,78 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
 
-  describe "#update_quality" do
+  describe '#update_quality' do
 
-    context "Aged Brie" do
+    context 'Aged Brie' do
 
       let(:aged_brie) {
         Item.new('Aged Brie', 10, 10)
       }
 
-      it "increases in quality at the end of each day" do
+      it 'increases in quality at the end of each day' do
         GildedRose.new([aged_brie]).update_quality
         expect(aged_brie.quality).to eq(11)
       end
 
-      it "decreases sell by date at the end of each day" do
+      it 'decreases sell by date at the end of each day' do
         GildedRose.new([aged_brie]).update_quality
         expect(aged_brie.sell_in).to eq(9)
       end
 
-      it "never increases the quality to over 50" do
+      it 'never increases the quality to over 50' do
         aged_brie = Item.new('Aged Brie', 10, 50)
         GildedRose.new([aged_brie]).update_quality
         expect(aged_brie.quality).to eq(50)
       end
 
-      it "increases quality twice as fast at sell by date" do
+      it 'increases quality twice as fast at sell by date' do
         aged_brie = Item.new('Aged Brie', 0, 15)
         GildedRose.new([aged_brie]).update_quality
         expect(aged_brie.quality).to eq(17)
       end
 
-      it "increases quality twice as fast after sell by date" do
+      it 'increases quality twice as fast after sell by date' do
         aged_brie = Item.new('Aged Brie', -1, 15)
         GildedRose.new([aged_brie]).update_quality
         expect(aged_brie.quality).to eq(17)
       end
     end
 
-    context "Backstage passes" do
-      it "increases in Quality by 1 when there are 11 days left" do
+    context 'Backstage passes' do
+      it 'increases in Quality by 1 when there are 11 days left to the concert' do
         backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 15)
         GildedRose.new([backstage_passes]).update_quality
         expect(backstage_passes.quality).to eq(16)
       end
 
-      it "increases in Quality by 2 when there are 10-6 days left" do
+      it 'increases in Quality by 2 when there are 10-6 days left to the concert' do
         backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 15)
         GildedRose.new([backstage_passes]).update_quality
         expect(backstage_passes.quality).to eq(17)
       end
 
-      it "increases in Quality by 2 when there are 6 days left" do
+      it 'increases in Quality by 2 when there are 6 days left to the concert' do
         backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', 6, 15)
         GildedRose.new([backstage_passes]).update_quality
         expect(backstage_passes.quality).to eq(17)
       end
 
-      it "increases in Quality by 3 when there are 5 days" do
+      it 'increases in Quality by 3 when there are 5 days left to the concert' do
         backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 15)
         GildedRose.new([backstage_passes]).update_quality
         expect(backstage_passes.quality).to eq(18)
       end
 
-      it "increases in Quality by 3 when there are less than 5 days" do
-        backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', rand(4) + 1, 15)
+      it 'increases in Quality by 3 when there are less than 5 days left to the concert' do
+        backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', 3, 15)
         GildedRose.new([backstage_passes]).update_quality
         expect(backstage_passes.quality).to eq(18)
-        # (1..4).each do |s|
-        #   backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', s, 15)
-        #   GildedRose.new([backstage_passes]).update_quality
-        #   expect(backstage_passes.quality).to eq(18)
-        # end
+      end
+
+      it 'drops Quality to 0 after the concert' do
+        backstage_passes = Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 20)
+        GildedRose.new([backstage_passes]).update_quality
+        expect(backstage_passes.quality).to eq(0)
       end
     end
   end
