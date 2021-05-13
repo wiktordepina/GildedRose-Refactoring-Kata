f9a0d6db63711a285975e93bad27c3ab4d792726
Wiktor de Pina <Wiktor.Depina@simplybusiness.co.uk>
2021-05-13 11:01:00 +0100

    refactored to use unless instead of if not

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -24,7 +24,7 @@ class GildedRose
         item.increase_quality
         if item.name == "Backstage passes to a TAFKAL80ETC concert"
           if item.sell_in < 11
-            if item.quality < 50
+            unless item.max_quality?
               item.increase_quality
             end
           end
