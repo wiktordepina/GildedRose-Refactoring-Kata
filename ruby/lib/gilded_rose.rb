abcec319f35e82e224b4aaef465039d14e356cbf
Wiktor de Pina <Wiktor.Depina@simplybusiness.co.uk>
2021-05-13 10:55:56 +0100

    refactored to use decrease_quality method

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -16,7 +16,7 @@ class GildedRose
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
         if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
+          item.decrease_quality
         end
       end
     else
